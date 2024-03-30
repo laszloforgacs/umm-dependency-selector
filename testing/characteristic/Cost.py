f020193ae5012d8925249811443d09df1719c3cc
lfor <laszlo.forgacs798@gmail.com>
2024-03-30 11:27:51 +0100

    Added Cost characteristic

---
+++ testing/characteristic/Cost.py
@@ -0,0 +1,8 @@
+from domain.model.Characteristic import Characteristic
+from domain.model.SubCharacteristic import SubCharacteristic
+from presentation.viewpoint_preferences.ComponentPreferencesState import PrefMatrix
+
+
+class Cost(Characteristic[float]):
+    def __init__(self, children: dict[str, SubCharacteristic] = {}, preference_matrix: PrefMatrix = {}):
+        super().__init__("Cost", children, preference_matrix=preference_matrix)

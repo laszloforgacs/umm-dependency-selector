954b8f446db55a5873e6e95cf4200d602dc66a1d
lfor <laszlo.forgacs798@gmail.com>
2024-03-31 13:13:38 +0200

    Adjusted License visitor

--- testing/visitors/license/OSSAQMLicenseVisitor.py
+++ testing/visitors/license/OSSAQMLicenseVisitor.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from github.Repository import Repository
 
 from presentation.core.visitors.Visitor import Visitor
@@ -9,26 +11,33 @@ class OSSAQMLicenseVisitor(Visitor[int]):
 
     async def measure(self, measure: 'BaseMeasure', repository: Repository) -> int:
         try:
-            scoring_table = {
-                "none": 1,
-                "other": 1,
-                "gpl-2.0": 2,
-                "lgpl-2.1": 2,
-                "mpl-2.0": 3,
-                "gpl-3.0": 3,
-                "epl-2.0": 3,
-                "agpl-3.0": 3,
-                "cc0-1.0": 4,
-                "bsl-1.0": 4,
-                "bsd-2-clause": 4,
-                "bsd-3-clause": 4,
-                "unlicense": 4,
-                "mit": 5,
-                "apache-2.0": 5,
-            }
-            license = repository.license
-            print(f"{repository.full_name}: {measure.name} is {license}")
-            return scoring_table.get(license, 1)
+            if repository.license is None:
+                print(f"{repository.full_name}: {measure.name} is None. Score: 1")
+                return 1
+            score = self._get_score(repository.license.key)
+            print(f"{repository.full_name}: {measure.name} is {repository.license}. Score: {score}")
+            return score
 
         except Exception as e:
             raise Exception(str(e))
+
+    def _get_score(self, license_key: Optional[str]) -> int:
+        scoring_table = {
+            "none": 1,
+            "other": 1,
+            "gpl-2.0": 2,
+            "lgpl-2.1": 2,
+            "mpl-2.0": 3,
+            "gpl-3.0": 3,
+            "epl-2.0": 3,
+            "agpl-3.0": 3,
+            "cc0-1.0": 4,
+            "bsl-1.0": 4,
+            "bsd-2-clause": 4,
+            "bsd-3-clause": 4,
+            "unlicense": 4,
+            "mit": 5,
+            "apache-2.0": 5,
+        }
+
+        return scoring_table.get(license_key, 1)

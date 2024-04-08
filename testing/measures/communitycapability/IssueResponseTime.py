1634386baff52e040830f1a3ab35a5a56e43a1ca
lfor <laszlo.forgacs798@gmail.com>
2024-04-08 12:03:40 +0200

    Added IssueResponseTime

---
+++ testing/measures/communitycapability/IssueResponseTime.py
@@ -0,0 +1,6 @@
+from domain.model.Measure import BaseMeasure
+
+
+class IssueResponseTime(BaseMeasure[float]):
+    def __init__(self, visitor=None):
+        super().__init__("Issue Response Time", "issue", 1, visitor)

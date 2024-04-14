5ad98dcc7bf781eb049072fb265c82aa6b282f89
lfor <laszlo.forgacs798@gmail.com>
2024-04-14 14:27:25 +0200

    Added OpenFeatureRequests from Cruz

---
+++ testing/subcharacteristic/openfeaturerequests/CruzOpenFeatureRequests.py
@@ -0,0 +1,7 @@
+from domain.model.MeasureableConcept import MeasurableConcept
+from domain.model.SubCharacteristic import SubCharacteristic
+
+
+class CruzOpenFeatureRequests(SubCharacteristic[list[int]]):
+    def __init__(self, children: dict[str, MeasurableConcept] = {}):
+        super().__init__("CruzOpenFeatureRequests", children)

d5445b184384ba788184a4328998bc40a0eaf3d9
lfor <laszlo.forgacs798@gmail.com>
2024-04-14 15:12:11 +0200

    Added OpenFeatureRequestCount to VisitorFactory

--- testing/visitors/VisitorFactory.py
+++ testing/visitors/VisitorFactory.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from testing.measures.number_of_open_feature_request.OpenFeatureRequestCountVisitor import \
+    OpenFeatureRequestCountVisitor
 from testing.measures.numberofreleases.ReleaseCountVisitor import ReleaseCountVisitor
 from testing.subcharacteristic.communitycapability.AugurClosedIssuesCountVisitor import AugurClosedIssuesCountVisitor
 from testing.subcharacteristic.communitycapability.AugurIssueThroughputVisitor import AugurIssueThroughputVisitor
@@ -39,6 +41,7 @@ class MeasureVisitorFactory(VisitorFactory):
         self.visitor_mappings = {
             # Key needs to be the exact name of the class, not the class property "name"
             # Add more mappings as needed
+            "OpenFeatureRequestCount": OpenFeatureRequestCountVisitor,
             "ReleaseCount": ReleaseCountVisitor,
             "IssueResponseTime": AugurIssueResponseTimeVisitor,
             "ClosedIssueResolutionDuration": AugurClosedIssueResolutionDurationVisitor,
@@ -105,6 +108,7 @@ class MeasurableConceptVisitorFactory(VisitorFactory):
         self.visitor_mappings = {
             # Key needs to be the exact name of the class, not the class property "name"
             # Add more mappings as needed
+            "NumberOfOpenFeatureRequests": (NoOpNormalizeVisitor, AddAggregateVisitor),
             "NumberOfReleases": (NoOpNormalizeVisitor, AddAggregateVisitor),
             "TimeToRespondToIssues": (NoOpNormalizeVisitor, AddAggregateVisitor),
             "DurationToCloseIssuesMC": (NoOpNormalizeVisitor, AddAggregateVisitor),

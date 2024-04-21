4c1b459edc102c771c93f3eea0f88bd72cd8e804
lfor <laszlo.forgacs798@gmail.com>
2024-04-21 15:30:50 +0200

    Mock result

--- testing/visitors/communitycapability/TruckFactorVisitor.py
+++ testing/visitors/communitycapability/TruckFactorVisitor.py
@@ -14,8 +14,8 @@ class TruckFactorVisitor(Visitor[int]):
         try:
             base_path = os.getcwd()
             path_to_repository = f"{base_path}/{SOURCE_TEMP_DIR}/{repository.name}"
-            tf, commit_shas, authors = main(path_to_repository)
-            print(f"{repository.full_name}: {measure.name} is {tf}")
-            return tf
+            #tf, commit_shas, authors = main(path_to_repository)
+            #print(f"{repository.full_name}: {measure.name} is {tf}")
+            return 1
         except Exception as e:
             raise Exception(str(e))

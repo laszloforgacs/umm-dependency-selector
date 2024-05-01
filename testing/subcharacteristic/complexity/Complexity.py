25eb53f10ce197ac1aeee11a794fef60db6cd8fd
lfor <laszlo.forgacs798@gmail.com>
2024-05-01 16:29:20 +0200

    Removed mock measures

--- testing/subcharacteristic/complexity/Complexity.py
+++ testing/subcharacteristic/complexity/Complexity.py
@@ -4,4 +4,4 @@ from domain.model.SubCharacteristic import SubCharacteristic
 
 class Complexity(SubCharacteristic[list[float]]):
     def __init__(self, children: dict[str, MeasurableConcept] = {}):
-        super().__init__("Complexity - by Mijinyawa et al.", children)
+        super().__init__("Complexity", children)
